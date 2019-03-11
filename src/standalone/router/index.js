ffd3c5183dccd8f657715b9a5f5d6c8c1ff9be61
Eric Dunaway <edunaway@techtonic.com>
2019-03-11 14:30:05 -0600

    add initial vuetify page to about

--- src/standalone/router/index.js
+++ src/standalone/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '../../plugins/vuetify';
 
 Vue.use(VueRouter);
 
