aa8216a70dc5d04b96add2ad87feda0aac514327
Eric Dunaway <edunaway@techtonic.com>
2019-03-11 14:30:05 -0600

    add initial vuetify page to about

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,4 @@ import 'vuetify/src/stylus/app.styl';
 
 Vue.use(Vuetify, {
   iconfont: 'md',
-  theme: {
-    primary: '#accc20',
-  },
 });
---
+++ src/standalone/components/Time.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout justify-center>
+    <div id="clock">
+      <!-- {{ message }} -->
+    </div>
+  </v-layout>
+</template>
+
+<script>
+export default {
+//   data: () => ({
+//     message: 'Current Time:',
+//     currentTime: null,
+//   }),
+//   created() {
+//     this.currentTime = moment().format('LTS');
+//     setInterval(() => this.updateCurrentTime(), 1 * 1000);
+//   },
+//   methods: {
+//     updateCurrentTime() {
+//       this.currentTime = moment().format('LTS');
+//     },
+//   },
+};
+</script>
+
+<style scoped>
+#clock {
+  font-size: 4em;
+  text-align: center;
+}
+</style>
