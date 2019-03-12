a7b14d62f0cb18264c98dd5ef67b84a8e45b2d5d
Megan M <mmacdonald@techtonicgroup.com>
2019-03-12 17:43:28 -0600

    added techtonic logo and began layout component

--- src/standalone/router/pages/Index.vue
+++ src/standalone/router/pages/Index.vue
@@ -1,33 +1,35 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <v-toolbar-title class="headline">
+        <span>Techtonic</span>
       </v-toolbar-title>
       <v-spacer />
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://www.techtonic.com"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
+        <v-img
+          :src="require('../../../assets/Techtonic_Black.png')"
+          class="my-3"
+          contain
+        />
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld />
+      <Layout />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from '../../components/HelloWorld';
+import Layout from '../../components/Layout';
 
 export default {
   components: {
-    HelloWorld,
+    Layout,
   },
   data() {
     return {};
