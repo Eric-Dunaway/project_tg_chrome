9c88d87fee84b7494c6d9d8cbd7b1da69b2a9f78
ivan tarin <itarin>
2019-03-12 12:01:17 -0600

    Background Component (#3)

---
+++ src/standalone/components/MiddleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    id="middleComp"
+  >
+    <slot />
+  </button>
+</template>
+<script>
+export default {
+  name: 'Button',
+};
+</script>
+<style>
+  #middleComp {
+    height: 100px;
+    width: 250px;
+    background-color: white;
+    border-radius: 30px;
+    font-size: 35px;
+    opacity: .68;
+  }
+</style>
