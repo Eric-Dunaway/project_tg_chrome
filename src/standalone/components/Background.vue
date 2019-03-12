abd9395efd5bd9b38339f2ee76723ddfd353f3e5
ivan tarin <itarin>
2019-03-12 12:01:17 -0600

    got background image in div finally, not background yet

---
+++ src/standalone/components/Background.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="picture">
+    <v-container>
+      <v-layout justify-space-around>
+        <v-flex xs5>
+          <div class="title mb-1">
+            Default (cover)
+          </div>
+          <v-layout column>
+            <div class="subheading">
+              Matching
+            </div>
+            <v-img src="https://picsum.photos/510/300?random" />
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+<script type="application/javascript">
+// import Unsplash from 'unsplash-js';
+
+// const unsplash = new Unsplash({
+//   applicationId: '{c01fec69a3d7aba9294659ebf752717ae5bbbe7283df275c2195af91d67de6f1}',
+//   secret: '{6971f63c5bf6731860063a4cf1eabb65fcef1d7ab9b6bbc632f7cd4aeaed1e2d}'
+// });
+export default {
+  data: () => ({
+  }),
+};
+</script>
+
+<style>
+  #picture {
+    background-image: url("../../assets/hero.jpeg");
+    background-size: cover;
+  }
+</style>
--- src/standalone/router/pages/Index.vue
+++ src/standalone/router/pages/Index.vue
@@ -1,16 +1,33 @@
-<template>
-  <v-container>
-    <h1>Main container content here</h1>
-    <!-- Components go here (Clock, calendar, Profile/settings) -->
-  </v-container>
+<template v-bind:style="{ backgroundImage: 'url(' + image + ')' }">
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>open_in_new</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <Background />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-// import components here
+import Background from '../../components/Background';
 
 export default {
   components: {
-    // register components for the Main Page here
+    Background,
   },
   data() {
     return {};
