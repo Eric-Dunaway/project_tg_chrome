eef31d8ce5c0dfa9ef7ca253570953c242dfee03
ivan tarin <itarin>
2019-03-15 13:54:00 -0600

    addded container, might remove later, detecting screen size

--- src/standalone/components/Background.vue
+++ src/standalone/components/Background.vue
@@ -1,14 +1,26 @@
 <template>
-  <v-card>
-    <v-img
-      :src="image"
-      tile
-      aspect-ratio="1.7"
-      height="100%"
+  <v-container
+    fluid
+  >
+    <v-layout
+      row
+      wrap
     >
-      <!-- component -->
-    </v-img>
-  </v-card>
+      <v-flex grow>
+        <v-card>
+          <v-img
+            :src="image"
+            tile
+            aspect-ratio="1.7"
+            height="100%"
+            fill-height
+          >
+            <!-- component -->
+          </v-img>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script type="application/javascript">
 import axios from 'axios';
@@ -26,15 +38,22 @@ export default {
           },
           params: {
             query: 'jellyfish',
+            orientation: 'landscape',
+            w: window.screen.width,
+            h: window.screen.height,
           },
         });
         console.log(response);
+        console.log();
         this.image = response.data.urls.full;
       } catch (error) {
         console.log('Line 34 AXIOS ERROR', error);
+        this.image = '../../assets/hero.jpeg';
       }
     };
     picture();
+    console.log('WIDTH!', window.screen.width);
+    console.log('HEIGHT!', window.screen.height);
   },
 };
 
