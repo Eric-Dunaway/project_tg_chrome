6303fe0dcb30e366d0f5801c850285e6909a747f
ivan tarin <itarin>
2019-03-15 10:51:49 -0600

    added axios and finally got data passing through by a prop

--- src/standalone/components/Background.vue
+++ src/standalone/components/Background.vue
@@ -4,25 +4,36 @@
       :src="image"
       tile
       aspect-ratio="1.7"
+      height="100%"
     >
       <!-- component -->
     </v-img>
   </v-card>
 </template>
 <script type="application/javascript">
-//
-// import Unsplash from 'unsplash-js';
+import axios from 'axios';
 
-// const unsplash = new Unsplash({
-//   applicationId: '{c01fec69a3d7aba9294659ebf752717ae5bbbe7283df275c2195af91d67de6f1}',
-//   secret: '{6971f63c5bf6731860063a4cf1eabb65fcef1d7ab9b6bbc632f7cd4aeaed1e2d}'
-// });
-const image = 'https://images.unsplash.com/flagged/photo-1552345301-833de8c1e8c1?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80';
 export default {
   data: () => ({
-    image,
+    image: '',
   }),
+  created() {
+    axios.get('https://api.unsplash.com/search/photos?query=jellyfish', {
+      headers: {
+        Authorization: 'Client-ID c01fec69a3d7aba9294659ebf752717ae5bbbe7283df275c2195af91d67de6f1',
+      },
+    })
+      .then((response) => {
+      // JSON responses are automatically parsed.
+        this.image = response.data.results[1].urls.full;
+        console.log(response.data.results[0].urls.full);
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
 };
+
 </script>
 
 <style>
