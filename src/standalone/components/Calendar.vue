959985b9027dc4dd7d34224cc2a3513977c64409
Megan M <mmacdonald@techtonicgroup.com>
2019-03-19 09:15:05 -0600

    begin layout of calendar component

---
+++ src/standalone/components/Calendar.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-layout class="calendar">
+    <v-flex>
+      <h1 class="title">
+        Here's Johnny
+      </h1>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        class="elevation-1"
+      >
+        <template v-slot:items="props">
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-right">
+            {{ props.item.calories }}
+          </td>
+          <td class="text-xs-right">
+            {{ props.item.fat }}
+          </td>
+          <td class="text-xs-right">
+            {{ props.item.carbs }}
+          </td>
+          <td class="text-xs-right">
+            {{ props.item.protein }}
+          </td>
+          <td class="text-xs-right">
+            {{ props.item.iron }}
+          </td>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    events: [
+      { time: '1:30', title: 'Backlog' },
+    ],
+
+    headers: [
+      {
+        text: 'Dessert (100g serving)',
+        align: 'left',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Calories', value: 'calories' },
+      { text: 'Fat (g)', value: 'fat' },
+      { text: 'Carbs (g)', value: 'carbs' },
+      { text: 'Protein (g)', value: 'protein' },
+      { text: 'Iron (%)', value: 'iron' },
+    ],
+    desserts: [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: '1%',
+      },
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: '1%',
+      },
+    ],
+
+
+  }),
+};
+</script>
+
+<style>
+.calendar {
+  border: 1px solid red;
+}
+.title {
+  border: 1px solid blue;
+  max-width: 50%;
+}
+
+</style>
--- src/standalone/router/pages/Index.vue
+++ src/standalone/router/pages/Index.vue
@@ -1,16 +1,40 @@
 <template>
-  <v-container>
+  <v-container
+    align-content-center
+    justify-center
+  >
     <h1>Main container content here</h1>
     <!-- Components go here (Clock, calendar, Profile/settings) -->
+    <v-layout
+      align-center
+      justify-center
+      row
+      fill-height
+      class="calendar-container"
+    >
+      <p class="clock pa-2">
+        This is a Clock
+      </p>
+    </v-layout>
+    <v-layout
+      align-center
+      justify-center
+      row
+      fill-height
+    >
+      <Calendar />
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 // import components here
+import Calendar from '../../components/Calendar';
 
 export default {
   components: {
     // register components for the Main Page here
+    Calendar,
   },
   data() {
     return {};
@@ -27,7 +51,13 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 40px;
+.clock {
+  font-size: 80px;
+  height: 40%;
+  margin-top: 20%;
+  border: 1px solid black;
+}
+.calendar-container {
+  border: 1px solid green;
 }
 </style>
