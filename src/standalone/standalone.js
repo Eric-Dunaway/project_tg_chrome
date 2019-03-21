f6ac9083fa55debf06b05b4f913f2555c269ef7b
Eric Dunaway <36860672+Eric-Dunaway@users.noreply.github.com>
2019-03-21 10:02:33 -0600

    Build enhancements (#4)
    * added build config and optimized font usage
    
    * optimized firebase imports

--- src/standalone/standalone.js
+++ src/standalone/standalone.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import App from './App';
 import store from '../store';
 import router from './router';
